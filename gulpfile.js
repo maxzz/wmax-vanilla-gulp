9006154bb37c5df7a9e8da7ad0cde8287ecce7e1
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-18 14:40:03 -0800

    WIP. gulp. add taskTypescript()

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,20 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const sass = require('gulp-sass')(require('sass'));
 
+/////////////////////////////////////////////////////////////////////////////
+
+let tsProject;
+
+function taskTypescript() {
+    !tsProject && (tsProject = ts.createProject('tsconfig.json'));
+    return tsProject
+        .src()
+        .pipe(tsProject())
+        .pipe(gulp.dest(rootDest));
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
 function pipeTraceTask() {
     return through(function transform(file, enc, callback) {
         console.log('trace task filename:', file.basename);
@@ -12,9 +26,9 @@ function pipeTraceTask() {
     });
 }
 
-/////////////////
+/////////////////////////////////////////////////////////////////////////////
 
-var paths = {
+const paths = {
     pages: ["src/*.html"],
 };
 
@@ -22,12 +36,9 @@ gulp.task("copy-html", function () {
     return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
 
-///////////
+/////////////////////////////////////////////////////////////////////////////
 
-// Run:
-// gulp sass
-// Complies sass only once
-gulp.task('sass', function () {
+gulp.task('compile-sass', function () {
     const processors = [tailwindcss,];
     return gulp.src('./scss/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -35,7 +46,7 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('prod', function () {
+gulp.task('compile-sass-prod', function () {
     const processors = [tailwindcss, autoprefixer, cssnano];
     return gulp.src('./scss/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -43,9 +54,10 @@ gulp.task('prod', function () {
         .pipe(gulp.dest('./css'));
 });
 
-// Run:
-// gulp watch
-// Starts watcher and keeps compiling the css file as you make changes
 gulp.task('watch', function () {
-    gulp.watch("./**/*.scss", gulp.series('prod'));
+    gulp.watch("./**/*.scss", gulp.series('compile-sass-prod'));
 });
+
+/////////////////////////////////////////////////////////////////////////////
+
+exports.ts = taskTypescript;
